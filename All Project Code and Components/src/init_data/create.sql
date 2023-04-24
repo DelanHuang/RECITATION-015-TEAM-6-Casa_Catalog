0fa4852d26f1dd5659529ffdffdf465f0604eeeb
Andrew Zirger <azirger3@gmail.com>
2023-04-23 23:43:43 -0600

    Revert "Added price updating functionality to watchlist"
    This reverts commit 005760d58ee92dfea59176f8dacc92bf25d937e7.

--- All Project Code and Components/src/init_data/create.sql
+++ All Project Code and Components/src/init_data/create.sql
@@ -3,33 +3,31 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     userId SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL, 
-    password CHAR(60) NOT NULL
+        password CHAR(60) NOT NULL,
 );
-
 CREATE TABLE watchlist (
   id SERIAL PRIMARY KEY,
   userId INTEGER NOT NULL,
-  productId BIGINT NOT NULL,
+  productId INTEGER NOT NULL,
   itemImage TEXT NOT NULL,
   itemName TEXT NOT NULL,
   itemPrice NUMERIC(10, 2) NOT NULL,
-  itemUrl TEXT NOT NULL,
-  watchPrice NUMERIC(10, 2)
+  itemUrl TEXT NOT NULL
 );
 
 -- contains all the inventories created by the user 
 DROP TABLE IF EXISTS inventory CASCADE;
 CREATE TABLE inventory(
-    inventory_id SERIAL PRIMARY KEY,
-    inventory_name VARCHAR(50) NOT NULL
+    inventory_id SERIAL PRIMARY KEY NOT NULL
+    inventory_name VARCHAR(50) PRIMARY KEY NOT NULL,
 );
 
 -- holds all items in a given inventory
 DROP TABLE IF EXISTS product CASCADE;
 CREATE TABLE products(
-    product_id SERIAL PRIMARY KEY,
+    product_id SERIAL PRIMARY KEY NOT NULL
     product_name VARCHAR(50) NOT NULL,
-    product_category VARCHAR(50), 
+    product_catagory VARCHAR(50), 
     product_quantity INT,
     last_price NUMERIC,
     last_purchase DATE
@@ -38,17 +36,17 @@ CREATE TABLE products(
 -- holds all items we have scraped 
 DROP TABLE IF EXISTS all_products CASCADE;
 CREATE TABLE all_products(
-    all_product_id SERIAL PRIMARY KEY,
-    all_product_name VARCHAR(50) NOT NULL,
-    all_product_category VARCHAR(50), 
-    current_price NUMERIC
+    all_product_id SERIAL PRIMARY KEY NOT NULL,
+    all_product_name VARCHAR(50) NOT NULL
+    all_product_catagory VARCHAR(50) 
+    current_price NUMERIC,
 );
 
 DROP TABLE IF EXISTS user_to_inventories CASCADE;
 CREATE TABLE user_to_inventories(
     user_id INT NOT NULL,
     inventory_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(userId),
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (inventory_id) REFERENCES inventory(inventory_id)
 );
 
@@ -56,14 +54,14 @@ DROP TABLE IF EXISTS inventory_to_products CASCADE;
 CREATE TABLE inventory_to_products(
     product_id INT NOT NULL,
     inventory_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
     FOREIGN KEY (inventory_id) REFERENCES inventory(inventory_id)
 );
 
 DROP TABLE IF EXISTS products_to_all_products CASCADE;
-CREATE TABLE products_to_all_products(
+CREATE TABLE products_to__all_products(
     product_id INT NOT NULL,
     all_product_id INT NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(product_id),
-    FOREIGN KEY (all_product_id) REFERENCES all_products(all_product_id)
+    FOREIGN KEY (all_product_id) REFERENCES all_product(all_product_id)
 );
