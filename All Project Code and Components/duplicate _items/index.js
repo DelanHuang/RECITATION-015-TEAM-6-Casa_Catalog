06335bb7710156eb020c60e736ab9ddb77b8fb67
sookki9205 <sookki9205@gmail.com>
2023-04-24 00:33:05 -0600

    s

---
+++ All Project Code and Components/duplicate _items/index.js
@@ -0,0 +1,181 @@
+const express = require('express'); // This is used to build an application server
+const app = express();
+const pgp = require('pg-promise')(); // This is used to connect to the Postgres DB
+const bodyParser = require('body-parser');
+const session = require('express-session'); // Used to set the session object
+const bcrypt = require('bcrypt'); // Used for hashing passwords
+const axios = require('axios'); // Used to make HTTP requests
+app.use(express.static('All Project Code and Components'));
+
+// Setting up the database
+
+// Configuring the database
+const dbConfig = {
+  host: 'db', // the database server
+  port: 5432, // the database port from docker-compose.yaml
+  database: process.env.POSTGRES_DB, // the database name (These three are found in the .env file)
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// Making sure the database connection was successful
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); 
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
+
+
+
+app.set('view engine', 'ejs'); // set the view engine to EJS
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+// initialize session variables
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+// Creating the user variable
+
+const user = {
+    username: undefined,
+    password: undefined,
+  };
+
+  const inventory = {
+    inventory_id : undefined,
+    inventory_name : undefined
+  }
+//API Integration
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    // Default to login page.
+    return res.redirect('/login');
+  }
+  next();
+};
+
+app.get('/', (req, res) => {
+  res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+  res.render('pages/home');
+});
+  
+app.get('/login', (req, res) => {
+  res.render('pages/login');
+});
+  
+app.get('/register', (req, res) => {
+  res.render('pages/register');
+});
+    
+app.post('/register', async (req, res) => {
+  const username = req.body.input_username;
+  const password = req.body.input_password;
+  try {
+    // Hash the password using bcrypt
+    const hashedPassword = await bcrypt.hash(req.body.input_password, 10);
+
+    // Insert username and hashed password into 'users' table
+    const result = await db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
+
+    // Redirect to /login page after successful insert
+    res.redirect( '/login' );
+  } catch (error) {
+    // Render the register page with an error message if the insert fails
+    res.status(400).render('pages/register', { error: 'An error occurred while registering. Please try again.' });
+  }
+});
+      
+app.post('/login', async (req, res) => {
+  //const { username, password } = req.body;
+  const username = req.body.input_username;
+  const password = req.body.input_password;
+
+  try {
+    // Find the user with the given username
+    const [user] = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+
+    // If user is not found, redirect to register page
+    if (!user) {
+      res.redirect('/register');
+      return;
+    }
+
+    // Compare the entered password with the hashed password in the database
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      // If the password is incorrect, throw an error
+      throw new Error('Incorrect username or password.');
+    }
+
+    // Save the user in the session
+    req.session.user = user;
+    req.session.save();
+
+    // Redirect to the discover page after setting the session
+    res.redirect('/discover');
+  } catch (error) {
+    // Send an appropriate error message to the user and render the login page
+    res.status(401).render('pages/login', { error: error.message, message: "Incorrect username or password" });
+  }
+});
+
+app.get("/discover", (req, res) => {
+  const searchTerm = req.query.q || "Baseball Cards"; // default search term is "Baseball Cards"
+  axios.get(`https://svcs.ebay.com/services/search/FindingService/v1?Operation-Name=findItemsByKeywords&Service-Version=1.0.0&Security-AppName=AndrewZi-CasaCata-PRD-53ab496b1-879c446f&Response-Data-Format=JSON&REST-Payload&keywords=${encodeURIComponent(searchTerm)}`)
+    .then(results => {
+      const products = results.data.findItemsByKeywordsResponse[0].searchResult[0].item;
+      const items = products.map(product => {
+        const name = product.title[0];
+        const image = product.galleryURL[0];
+        const id = product.itemId[0];
+        const price = product.sellingStatus[0].currentPrice[0].__value__;
+        const url = product.viewItemURL[0];
+        return { name, image, id, price, url };
+      });
+      res.render("pages/discover", { items });
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
+app.get('/inventory', (req, res) => {
+  const query = 'select * from inventory';
+  db.any(query).then(data=>{
+    console.log(data);
+    res.render("pages/inventory",{
+      data: data[0]
+    })
+  });
+
+});
+  
+//Test route for lab 11
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
+
+
+// Starting the server
+
+module.exports = app.listen(3000); //For testing using command = npm run testandrun in docker-compose.yaml
+//app.listen(3000); //For running the application using command = npm start in docker-compose.yaml
+console.log('Server is listening on port 3000');
--- All Project Code and Components/duplicate _items/init_data/create.sql
+++ All Project Code and Components/duplicate _items/init_data/create.sql
@@ -1,54 +1,44 @@
 -- contains all users 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-    userId SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL, 
-    password CHAR(60) NOT NULL
-);
-
-DROP TABLE IF EXISTS watchlist CASCADE;
-CREATE TABLE watchlist (
-  id SERIAL PRIMARY KEY,
-  userId INTEGER NOT NULL,
-  productId INTEGER NOT NULL,
-  itemImage TEXT NOT NULL,
-  itemName TEXT NOT NULL,
-  itemPrice NUMERIC(10, 2) NOT NULL,
-  itemUrl TEXT NOT NULL
+DROP TABLE IF EXISTS user CASCADE;
+CREATE TABLE user(
+    user_id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(50) PRIMARY KEY,
+    password CHAR(60) NOT NULL,
 );
 
 -- contains all the inventories created by the user 
 DROP TABLE IF EXISTS inventory CASCADE;
 CREATE TABLE inventory(
     inventory_id SERIAL PRIMARY KEY NOT NULL,
-    inventory_name VARCHAR(50) NOT NULL
+    inventory_name VARCHAR(50) PRIMARY KEY NOT NULL
 );
 
 -- holds all items in a given inventory
 DROP TABLE IF EXISTS product CASCADE;
-CREATE TABLE products(
-    product_id SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE product(
+    product_id SERIAL PRIMARY KEY NOT NULL
     product_name VARCHAR(50) NOT NULL,
     product_catagory VARCHAR(50), 
     product_quantity INT,
     last_price NUMERIC,
     last_purchase DATE
+
 );
 
 -- holds all items we have scraped 
 DROP TABLE IF EXISTS all_products CASCADE;
 CREATE TABLE all_products(
     all_product_id SERIAL PRIMARY KEY NOT NULL,
-    all_product_name VARCHAR(50) NOT NULL,
-    all_product_catagory VARCHAR(50),
-    current_price NUMERIC
+    all_product_name VARCHAR(50) NOT NULL
+    all_product_catagory VARCHAR(50) 
+    current_price NUMERIC,
 );
 
 DROP TABLE IF EXISTS user_to_inventories CASCADE;
 CREATE TABLE user_to_inventories(
     user_id INT NOT NULL,
     inventory_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(userId),
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (inventory_id) REFERENCES inventory(inventory_id)
 );
 
@@ -56,7 +46,7 @@ DROP TABLE IF EXISTS inventory_to_products CASCADE;
 CREATE TABLE inventory_to_products(
     product_id INT NOT NULL,
     inventory_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
     FOREIGN KEY (inventory_id) REFERENCES inventory(inventory_id)
 );
 
@@ -65,5 +55,5 @@ CREATE TABLE products_to__all_products(
     product_id INT NOT NULL,
     all_product_id INT NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(product_id),
-    FOREIGN KEY (all_product_id) REFERENCES all_products(all_product_id)
+    FOREIGN KEY (all_product_id) REFERENCES all_product(all_product_id)
 );
